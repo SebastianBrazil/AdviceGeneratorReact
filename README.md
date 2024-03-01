70a871bd2ce406640961501971f1a2b5049e279e
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-03-01 12:49:14 -0800

    some stuff

--- README.md
+++ README.md
@@ -4,8 +4,8 @@ Sebastian Breve-Sanchez
 
 Advice Generator in React
 
-Desc
+Designed at 1440 by 900 pixels for desktop, 375 by 700 pixels for mobile. 
 
-3/1/24
+3/1/24: 
 
 Peer Review: 
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,26 +1,42 @@
 import React, { useEffect, useState } from 'react'
 import adviceFormat from '../interfaces/interfaces';
 import getAdvice from '../DataServices/DataServices';
+import bars from '../assets/images/pattern-divider-mobile.svg'
+import dice from '../assets/images/icon-dice.svg'
 
 const MainComponent = () => {
     const [advicePop, setAdvicePop] = useState<adviceFormat>();
+    const [isFlipped, setIsFlipped] = useState<boolean>(false);
 
     useEffect(() => {
         const adviceData = async () => {
-          const fetchedData = await getAdvice();
-          setAdvicePop(fetchedData);
+            const fetchedData = await getAdvice();
+            setAdvicePop(fetchedData);
         }
         adviceData();
-      }, []);
+    }, [isFlipped]);
+
+    const changeAd = () => {
+        setIsFlipped(!isFlipped);
+    }
 
     return (
         <div className='flex justify-center'>
-            <div className='w-11/12 lighterGrey text-center rounded-xl'>
-                {/* <p className='green mt-9 mb-6 fontChange fontWeightChange text-sm'>ADVICE #{advicePop?.slip.id}</p>
-                <p className='white mb-8 fontChange fontWeightChange text-2xl'>“{advicePop?.slip.advice}”</p> */}
-                <p className='green mt-9 mb-6 fontChange fontWeightChange text-sm tracking-[0.3em]'>ADVICE #117</p>
-                <p className='white mb-8 fontChange fontWeightChange text-2xl mx-5'>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p>
-                {/* <img src={} className='h-8 w-9' alt="" /> */}
+            <div className='w-11/12 mt-36 lighterGrey text-center rounded-xl'>
+                <p className='green mt-9 mb-6 fontChange fontWeightChange text-sm tracking-[0.3em]'>ADVICE #{advicePop?.slip.id}</p>
+                <p className='white mb-8 fontChange fontWeightChange text-2xl mx-5'>“{advicePop?.slip.advice}”</p>
+                {/* <p className='green mt-9 mb-6 fontChange fontWeightChange text-xs tracking-[0.3em]'>ADVICE #117</p>
+                <p className='white mb-6 fontChange fontWeightChange text-2xl mx-5'>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p> */}
+
+                <div className='flex justify-center'>
+                    <img src={bars} className='' alt="Center Divider Bars" />
+                </div>
+
+                <div className='flex justify-center'>
+                    <div onClick={changeAd} className='greenBG p-5 rounded-full relative top-8'>
+                        <img src={dice} className='' alt="Center Divider Bars" />
+                    </div>
+                </div>
             </div>
         </div>
     )
