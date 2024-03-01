958aa85493bcc5995f6b47e60e9cd30051383e24
SebastianBrazil <sbrevesanchez088@gmail.com>
2024-03-01 15:00:31 -0800

    everything but hover

--- src/DataServices/DataServices.ts
+++ src/DataServices/DataServices.ts
@@ -1,7 +1,8 @@
 import adviceFormat from "../interfaces/interfaces";
 
 const getAdvice = async () => {
-    const promise = await fetch('https://api.adviceslip.com/advice');
+    let randNum = Math.floor(Math.random() * 224) + 1
+    const promise = await fetch(`https://api.adviceslip.com/advice/${randNum}`);
     const data: adviceFormat = await promise.json();
     return data;
 }
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, createRef } from 'react'
 import adviceFormat from '../interfaces/interfaces';
 import getAdvice from '../DataServices/DataServices';
-import bars from '../assets/images/pattern-divider-mobile.svg'
 import dice from '../assets/images/icon-dice.svg'
 
 const MainComponent = () => {
@@ -22,19 +21,19 @@ const MainComponent = () => {
 
     return (
         <div className='flex justify-center'>
-            <div className='w-11/12 mt-36 lighterGrey text-center rounded-xl'>
-                <p className='green mt-9 mb-6 fontChange fontWeightChange text-sm tracking-[0.3em]'>ADVICE #{advicePop?.slip.id}</p>
-                <p className='white mb-8 fontChange fontWeightChange text-2xl mx-5'>“{advicePop?.slip.advice}”</p>
-                {/* <p className='green mt-9 mb-6 fontChange fontWeightChange text-xs tracking-[0.3em]'>ADVICE #117</p>
-                <p className='white mb-6 fontChange fontWeightChange text-2xl mx-5'>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p> */}
+            <div className='w-11/12 lg:w-5/12 mt-36 lg:mt-56 lighterGrey text-center rounded-xl'>
+                <p className='green mt-9 lg:mt-12 mb-6 lg:mb-9 fontChange fontWeightChange text-xs lg:text-base tracking-[0.3em]'>ADVICE #{advicePop?.slip.id}</p>
+                <p className='white mb-6 fontChange fontWeightChange text-2xl lg:text-3xl mx-5 lg:mx-12'>“{advicePop?.slip.advice}”</p>
+                {/* <p className='green mt-9 lg:mt-12 mb-6 lg:mb-9 fontChange fontWeightChange text-xs lg:text-base tracking-[0.3em]'>ADVICE #117</p>
+                <p className='white mb-6 fontChange fontWeightChange text-2xl lg:text-3xl mx-5 lg:mx-12'>“It is easy to sit up and take notice, what's difficult is getting up and taking action.”</p> */}
 
-                <div className='flex justify-center'>
-                    <img src={bars} className='' alt="Center Divider Bars" />
+                <div className='flex justify-center image'>
+                    <div className='imageBg'></div>
                 </div>
 
                 <div className='flex justify-center'>
-                    <div onClick={changeAd} className='greenBG p-5 rounded-full relative top-8'>
-                        <img src={dice} className='' alt="Center Divider Bars" />
+                    <div onClick={changeAd} className='greenBG p-5 lg:p-6 rounded-full relative top-8 lg:top-10 '>
+                        <img src={dice} className='lg:w-8' alt="Dice" />
                     </div>
                 </div>
             </div>
